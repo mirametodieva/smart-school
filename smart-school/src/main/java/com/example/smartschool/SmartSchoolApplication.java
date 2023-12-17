4b4e6dbe5001c826605ab603b412916ce1e53039
viliana0952 <133017284+viliana0952@users.noreply.github.com>
2023-12-17 14:05:48 +0200

    Create new spring project

---
+++ smart-school/src/main/java/com/example/smartschool/SmartSchoolApplication.java
@@ -0,0 +1,13 @@
+package com.example.smartschool;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class SmartSchoolApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(SmartSchoolApplication.class, args);
+	}
+
+}
