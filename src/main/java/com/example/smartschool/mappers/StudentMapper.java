8d8b0adbbd6c71f085623efd737f5f1437637c19
Mirela Metodieva <mirushankarr@gmail.com>
2024-01-01 23:25:40 +0200

    Create student's service, controller, repo and mapper

--- src/main/java/com/example/smartschool/mappers/StudentMapper.java
+++ src/main/java/com/example/smartschool/mappers/StudentMapper.java
@@ -1,4 +1,15 @@
 package com.example.smartschool.mappers;
 
-public class StudentMapper {
+import com.example.smartschool.dto.StudentDto;
+import com.example.smartschool.models.Student;
+import org.mapstruct.Mapper;
+import org.mapstruct.Mapping;
+
+@Mapper(componentModel = "spring")
+public interface StudentMapper {
+    @Mapping(target = "firstName", source = "dto.firstName")
+    @Mapping(target = "lastName", source = "dto.lastName")
+    @Mapping(target = "studentNum", source = "dto.studentNum")
+    @Mapping(target = "parentPhone", source = "dto.parentPhone")
+    Student convertDtoToEntity(StudentDto dto, Long id);
 }
--- src/main/java/com/example/smartschool/repositories/StudentRepo.java
+++ src/main/java/com/example/smartschool/repositories/StudentRepo.java
@@ -1,4 +1,21 @@
 package com.example.smartschool.repositories;
 
-public interface StudentRepo {
+import com.example.smartschool.models.Student;
+import org.springframework.data.jpa.repository.JpaRepository;
+import org.springframework.data.jpa.repository.Modifying;
+import org.springframework.data.jpa.repository.Query;
+import org.springframework.stereotype.Repository;
+
+import java.util.List;
+import java.util.Optional;
+
+@Repository
+public interface StudentRepo extends JpaRepository<Student, Long> {
+    Optional<Student> findStudentByStudentNum(Integer num);
+    List<Student> findStudentsByGradeName(String gradeName);
+    List<Student> findStudentsByGradeId(Long gradeId);
+
+    @Modifying
+    @Query("UPDATE Student SET grade_id = :gradeId WHERE id = :studentId")
+    void updateGrade(Long grade, Long studentId);
 }
